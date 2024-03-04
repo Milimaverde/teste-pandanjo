b0cfecdb117b7ebed2837259ab78ebc898e2ea94
Millena Limaverde <millenalimaverde@outlook.com>
2024-03-04 20:53:01 -0300

    fix: 🐛 svg star on assessment

--- js/modules/assessment.js
+++ js/modules/assessment.js
@@ -4,17 +4,16 @@ export default class Assessment {
   }
 
   async createStars(assessment) {
-    const starSVG = await fetch('../assets/img/star-icon.svg')
-      .then((response) => response.text())
-      .then((svgData) => svgData);
-
     for (let i = 1; i <= 5; i += 1) {
       const star = document.createElement('span');
       star.classList.add('assessment-stars__star');
       if (i > assessment) {
         star.classList.add('assessment-stars__star--empty');
       }
-      star.innerHTML = starSVG;
+      star.innerHTML = `
+      <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8.14286 0L9.97104 5.62658H15.8872L11.1009 9.10399L12.9291 14.7306L8.14286 11.2532L3.35661 14.7306L5.18479 9.10399L0.39854 5.62658H6.31467L8.14286 0Z" fill="#7A306C"/>
+      </svg>`;
       this.assessmentStarsContainer.appendChild(star);
     }
   }
